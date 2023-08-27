49274a3c25ffb18bc1873c934ff4480fe99a5010
Mohsin Riaz <wilddogers@gmail.com>
2023-08-27 02:51:25 +0200

    Refactor: Corrected and improved the code and added tests

---
+++ __tests__/index.test.ts
@@ -0,0 +1,130 @@
+import { expect, it, describe } from "vitest";
+import aliasClassName from "../lib";
+
+describe("alias-classname", () => {
+  it("should create an instance", () => {
+    const instance = aliasClassName();
+    expect(typeof instance === "function").toBeTruthy();
+  });
+
+  it("should have a debug function", () => {
+    const instance = aliasClassName();
+    expect(typeof instance.debug === "function").toBeTruthy();
+    const { aliases, classes } = instance.debug();
+    expect(typeof aliases).toBe("object");
+    expect(typeof classes).toBe("object");
+  });
+
+  it("Should filter out all the invalid classes", () => {
+    const instance = aliasClassName();
+    const classes = instance(
+      "class",
+      undefined,
+      "",
+      "  ",
+      false,
+      true,
+      null,
+      0,
+      234
+    );
+    expect(classes).toEqual("class");
+  });
+
+  it("Should filter out all the duplicated classes", () => {
+    const instance = aliasClassName();
+    const classes = instance("a", "a", "b", "b");
+    expect(classes).toEqual("a b");
+  });
+
+  it("should register default aliases", () => {
+    const instance = aliasClassName("base:class", "mod:modifier");
+    expect(instance("(base)")).toBe("class");
+    expect(instance("(mod)")).toBe("modifier");
+    expect(instance("(base)__(mod)")).toBe("class__modifier");
+    expect(instance("(base)__(mod)", "(mod)", "q")).toBe(
+      "class__modifier modifier q"
+    );
+    expect(instance("(random)")).toBe("(random)");
+  });
+
+  it("should validate the aliases when registering", () => {
+    [
+      { input: ["a:x"], output: { a: "x" } },
+      {
+        input: ["a:x", "b:(a)y", "c:(b)z", "q"],
+        output: { a: "x", b: "xy", c: "xyz" },
+      },
+      { input: [":"], output: {} },
+      { input: ["      :   "], output: {} },
+      { input: ["a:"], output: {} },
+      { input: [":b"], output: {} },
+      { input: ["a:b:c"], output: {} },
+      { input: ["a:(a)"], output: {} },
+      { input: ["(a):(b)"], output: {} },
+    ].forEach(({ input, output }) => {
+      const { aliases } = aliasClassName(...input).debug();
+      expect(aliases).toEqual(output);
+    });
+  });
+
+  describe("without aliases", () => {
+    it("should output the class passed in", () => {
+      const instance = aliasClassName();
+      expect(instance("a")).toBe("a");
+      expect(instance("b")).toBe("b");
+    });
+
+    it("should output the classes list passed in", () => {
+      const instance = aliasClassName();
+      expect(instance("a", "b")).toBe("a b");
+      expect(instance("a", "b", "c")).toBe("a b c");
+    });
+  });
+
+  describe("with aliases", () => {
+    it("should create an alias and output the passed in class", () => {
+      const instance = aliasClassName("a:x", "b:y");
+      expect(instance("a", "b")).toBe("a b");
+    });
+
+    it("should override alias value", () => {
+      const instance = aliasClassName();
+      expect(instance("a:x", "(a)")).toBe("x x");
+      expect(instance("a:y", "(a)")).toBe("y y");
+      expect(instance("a:z", "(a)")).toBe("z z");
+    });
+
+    it("should use an alias and output the composed class", () => {
+      const instance = aliasClassName("a:x");
+      const classes = instance("(a)__class");
+      expect(classes).toEqual("x__class");
+    });
+
+    it("should not resolve an alias if not formed correctly", () => {
+      const instance = aliasClassName();
+      expect(instance("base:(base)class")).toBe("(base)class");
+      expect(instance(":")).toBe("");
+      expect(instance("a:")).toBe("");
+      expect(instance(":b")).toBe("b");
+      expect(instance(" c  :b", "(c)")).toBe("b b");
+      expect(instance("a:(a)")).toBe("(a)");
+      expect(instance("(a):b")).toBe("b");
+      expect(instance("a:b:c")).toBe("c");
+    });
+
+    it("should create and alias of a composition of class and another alias", () => {
+      const instance = aliasClassName();
+      const classes = instance(
+        "base:class",
+        "mod:(base)__modifier",
+        "mod2:(mod)--variant",
+        "(mod2)--variant1"
+      );
+
+      expect(classes).toEqual(
+        "class class__modifier class__modifier--variant class__modifier--variant--variant1"
+      );
+    });
+  });
+});
--- lib/index.ts
+++ lib/index.ts
@@ -1,62 +1,105 @@
-const aliasDelimiterMatcher = /^.+:/;
-const aliasMatcher = /\(\w*\)/g;
+"use strict";
 
-export const validateString = (input: unknown) =>
-  typeof input === "string" && input.trim().length > 0;
+type AliasesStore = Record<string, string>;
+type ClassesStore = Record<string, string>;
 
-const resolveAliases = (aliases: Record<string, string>) => (input: string) => {
-  const aliasTokens = input.match(aliasMatcher);
-  if (aliasTokens?.length) {
-    return aliasTokens.reduce(
-      (output, alias) => output.replace(alias, aliases[alias]),
-      input
-    );
-  }
-  return input;
-};
+const aliasReferenceRegex = /\(\w*\)/g;
+const delimiterRegex = /:/g;
 
-const extractAliases = (acc: Record<string, string>, b: string) => {
-  if (!aliasDelimiterMatcher.test(b)) return acc;
-  const tokens = b.split(":");
-  if (tokens.length > 2)
-    throw Error("Found multiple alias delimiter (:) in the same input");
-  if (!tokens[0].trim().length || !tokens[1].trim().length)
-    throw Error("Found no alias or classname in the input");
-  return {
-    ...acc,
-    [`(${tokens[0]})`]: resolveAliases(acc)(tokens[1]),
+const trim = (input: string) => input.trim();
+const erode = (input: string, q = 1) => input.substring(q, input.length - q);
+const arrayToDict = (array: string[]) =>
+  array.reduce((acc, v) => ({ ...acc, [v]: v }), {});
+
+const validateInputs = (inputs: unknown[]): string[] => [
+  // remove duplicates using Set
+  ...new Set(
+    inputs.filter(
+      (input) => typeof input === "string" && input.trim().length > 0
+    ) as string[]
+  ),
+];
+
+const resolveAliases =
+  (aliasesStore: AliasesStore) => (target: Record<string, string>) => {
+    for (const key in target) {
+      if (!Object.prototype.hasOwnProperty.call(target, key)) continue;
+
+      const tokens = target[key].match(aliasReferenceRegex);
+      if (!tokens || !tokens.length) continue;
+
+      for (const aliasWithParentheses of tokens) {
+        const deParenthesisedAlias = erode(aliasWithParentheses);
+        if (!aliasesStore[deParenthesisedAlias]) continue; // should this throw an error?
+        target[key] = target[key].replace(
+          aliasWithParentheses,
+          aliasesStore[deParenthesisedAlias]
+        );
+      }
+    }
   };
+
+const getAliasClassPair = (input: string) => {
+  const delimiter = input.match(delimiterRegex);
+  // "a:b:c" is a no-no.
+  if (!delimiter || delimiter.length > 1) return null;
+
+  const [alias, className] = input.split(":").map(trim);
+  if (!alias.length || !className.length) return null;
+  // do not allow self referencing aliases. ex.: "a:(a)"
+  if (className.includes(`(${alias})`)) return null;
+  // do not allow an alias wrapped in parentheses. ex.: "(a):b"
+  if (alias.startsWith("(") && alias.endsWith(")")) return null;
+
+  return { [alias]: className };
 };
 
-const getUnAliasedClasses = (input: string) => {
-  if (!aliasDelimiterMatcher.test(input)) return input;
-  const tokens = input.split(":");
-  if (tokens.length > 2)
-    throw Error("Found multiple alias delimiter (:) in the same input");
-  if (!tokens[0].trim().length || !tokens[1].trim().length)
-    throw Error("Found no alias or classname in the input");
-  return tokens[1];
-};
-
-export const aliasClassName = (...defaultAliases: string[]) => {
-  let aliases = {};
-  let classNames = new Set<string>();
-  let classes = [];
-  return (...inputs: unknown[]) => {
-    (inputs.filter(validateString) as string[]).forEach(
-      classNames.add,
-      classNames
-    );
-
-    const classesArray = Array.from(classNames);
-
-    const extractedAliaes = classesArray.reduce(extractAliases, {});
-    aliases = { ...aliases, ...extractedAliaes };
-
-    classes = classesArray
-      .map(resolveAliases(aliases))
-      .map(getUnAliasedClasses);
-
-    return classes.join(" ");
+const registerAliases =
+  (
+    resolveAliasesFor: (store: Record<string, string>) => void,
+    aliasesStore: AliasesStore
+  ) =>
+  (inputs: string[]) => {
+    const validatedInput = validateInputs(inputs);
+    for (const input of validatedInput) {
+      const aliasClassPair = getAliasClassPair(input);
+      if (!aliasClassPair) continue;
+      Object.assign(aliasesStore, aliasClassPair);
+    }
+    resolveAliasesFor(aliasesStore);
   };
+
+const aliasClassName = (...aliases: string[]) => {
+  const aliasesStore: AliasesStore = {};
+  const classesStore: ClassesStore = {};
+  const resolveAliasesFor = resolveAliases(aliasesStore);
+  const register = registerAliases(resolveAliasesFor, aliasesStore);
+
+  if (aliases.length) register(aliases);
+
+  const resolveClasses = (...classnames: unknown[]) => {
+    const validatedClassnames = validateInputs(classnames);
+    Object.assign(classesStore, arrayToDict(validatedClassnames));
+
+    register(validatedClassnames);
+    resolveAliasesFor(classesStore);
+
+    return validatedClassnames
+      .map(
+        (classname) =>
+          classesStore[classname].split(delimiterRegex).reverse()[0]
+      )
+      .join(" ");
+  };
+
+  Object.defineProperty(resolveClasses, "debug", {
+    value: () => ({ aliases: aliasesStore, classes: classesStore }),
+    writable: false,
+    enumerable: false,
+    configurable: false,
+  });
+
+  return resolveClasses;
 };
+
+export default aliasClassName;
